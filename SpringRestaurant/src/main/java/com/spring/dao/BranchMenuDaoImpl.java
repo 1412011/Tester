a47dd9d0e5743ce07007656b0b0e5e571dcfc042
DESKTOP-7NCVNAM\HOANG ANH <nghoanganh19@gmail.com>
2017-12-31 14:28:24 +0700

    Config Hibernate and Dao service

---
+++ SpringRestaurant/src/main/java/com/spring/dao/BranchMenuDaoImpl.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.spring.dao;
+
+import com.spring.model.BranchMenu;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author HOANG ANH
+ */
+@Repository
+public class BranchMenuDaoImpl extends AbstractDao<Integer, BranchMenu> implements BranchMenuDao{
+    
+}
---
+++ SpringRestaurant/src/main/java/com/spring/dao/DishDaoImpl.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.spring.dao;
+
+import com.spring.model.Dish;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author HOANG ANH
+ */
+@Repository
+public class DishDaoImpl extends AbstractDao<Integer, Dish> implements DishDao{
+    
+}
